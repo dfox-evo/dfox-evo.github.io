2d47d4ae227f3b361c23b54a43c9107fa8ca98ac
Dylan Fox <dfox@ITs-MacBook-Pro.local>
2021-09-28 09:38:39 -0500

    update

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-analytics.ready(function() {
-    console.log("analytics is ready");
-  });
+function identify() {
+    analytics.identify('12345', {
+        name: 'dylan fox',
+        email: 'email@test.com'
+    }, () => {
+        console.log("identify call complete");
+    });
+}
+
+function track() {
+    analytics.track("visit", {
+        "title": "Hello Dylan"
+    }, () => {
+        console.log("track call complete");
+    });
+}
+
+// var link = document.getElementById('track-link');
+
+// analytics.trackLink(link, 'click evo link', () => {
+//     console.log('track link call complete');
+// });
+
+function page() {
+    analytics.page("home", {
+        "category": "home page"
+    }, () => {
+        console.log("page call complete");
+    });
+}
+
+function group() {
+    analytics.group('evo', () => {
+        console.log('group call complete');
+    });
+}
+
+function alias() {
+    analytics.alias('67890', '12345', () => {
+        console.log('alias call complete');
+    });
+}
