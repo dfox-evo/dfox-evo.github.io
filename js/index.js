7fc43c8368b64d56c23df0a731adf0a88c46d140
Dylan Fox <dfox@evolytics.com>
2022-10-25 13:52:18 -0500

    add prompt

--- js/index.js
+++ js/index.js
@@ -1,36 +1,41 @@
 function identify() {
-    analytics.identify('12345', {
-        name: 'dylan fox',
-        email: 'email@test.com'
-    }, () => {
-        console.log("identify call complete");
-    });
+  analytics.identify('12345', {
+    name: 'dylan fox',
+    email: 'email@test.com'
+  }, () => {
+    console.log("identify call complete");
+  });
 }
 
 function track() {
-    analytics.track("click", {
-        "title": "track click"
-    }, () => {
-        console.log("track call complete");
-    });
+  analytics.track("click", {
+    "title": "track click"
+  }, () => {
+    console.log("track call complete");
+  });
 }
 
 function page() {
-    analytics.page("home", {
-        "category": "home page"
-    }, () => {
-        console.log("page call complete");
-    });
+  analytics.page("home", {
+    "category": "home page"
+  }, () => {
+    console.log("page call complete");
+  });
 }
 
 function group() {
-    analytics.group('evo', () => {
-        console.log('group call complete');
-    });
+  analytics.group('evo', () => {
+    console.log('group call complete');
+  });
 }
 
 function alias() {
-    analytics.alias('67890', '12345', () => {
-        console.log('alias call complete');
-    });
+  analytics.alias('67890', '12345', () => {
+    console.log('alias call complete');
+  });
+}
+
+const prompt = () => {
+  var text;
+  var input = prompt("Please enter the Launch URL:", "https://assets.adobedtm.com/...");
 }
